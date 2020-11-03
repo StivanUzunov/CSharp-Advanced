60a0f74005d02857b97dc84ca06c9f62e28c96c1
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-03 20:35:10 +0200

    submit

---
+++ StackAndQueues/MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (input[0]==1)
+                {
+                    stack.Push(input[1]);
+                }
+                else if (input[0] == 2)
+                {
+                    if (stack.Count>0)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (input[0] == 3)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    
+                }
+                else if (input[0] == 4)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+            }
+            Console.WriteLine(String.Join(", ",stack));
+        }
+    }
+}
