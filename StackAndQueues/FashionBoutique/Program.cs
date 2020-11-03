c9c6a38280f7b27e1bed0e7de8dd8369cf410205
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-03 20:36:40 +0200

    submit

---
+++ StackAndQueues/FashionBoutique/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+            int capacityOfARack = int.Parse(Console.ReadLine());
+            int racksCounter = 1;
+            int sum = 0;
+            Stack<int> stack = new Stack<int>(clothes);
+
+            while (stack.Any())
+            {
+                if (capacityOfARack<sum+stack.Peek())
+                {
+                    racksCounter++;
+                    sum = 0;
+                }
+                sum += stack.Pop();
+
+            }
+            Console.WriteLine(racksCounter);
+        }
+    }
+}
