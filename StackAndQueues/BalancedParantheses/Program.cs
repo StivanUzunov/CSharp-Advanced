076db571ae76b0125dd03aedc3b7c1a85b3df8ef
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-03 20:40:15 +0200

    submit

---
+++ StackAndQueues/BalancedParantheses/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                var symbol = text[i];
+                if (stack.Any())
+                {
+                    if (symbol == '}' && stack.Peek() == '{')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (symbol == ')' && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else if (symbol == ']' && stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                }
+                stack.Push(symbol);
+            }
+            if (!stack.Any())
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
