43cba3853c18ed5c62dc7e7d227ea300f3290391
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-06 23:04:56 +0200

    submit

---
+++ SetsAndDictionaries/PeriodicTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicals = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemical = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int j = 0; j < chemical.Length; j++)
+                {
+                    chemicals.Add(chemical[j]);
+                }
+               
+            }
+            Console.WriteLine(String.Join(' ',chemicals));
+        }
+    }
+}
