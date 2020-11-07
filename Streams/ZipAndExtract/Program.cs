080c905441f32f8e47ef76a22e908831cbbcc5a6
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-11-07 19:48:33 +0200

    submit

---
+++ Streams/ZipAndExtract/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"D:\fileFolder",
+                @"D:\zipFolder\zipOfpng.zip");
+
+            ZipFile.ExtractToDirectory(@"D:\zipFolder\zipOfpng.zip", 
+                @"D:\extractFolder");
+        }
+    }
+}
