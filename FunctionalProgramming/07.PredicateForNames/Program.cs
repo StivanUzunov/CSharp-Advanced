1c436b60ab9b2822193698a10a49a686f9c8c8a8
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-12-07 14:02:06 +0200

    Adding files

---
+++ FunctionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            Func<List<string>, List<string>> function = (arr) =>
+             {
+                 List<string> filteredNames = new List<string>();
+                 for (int i = 0; i < arr.Count; i++)
+                 {
+                     if (arr[i].Length<=n)
+                     {
+                         filteredNames.Add(arr[i]);
+                     }
+                 }
+                 return filteredNames;
+             };
+            Console.WriteLine(String.Join(Environment.NewLine,function(names)));
+        }
+    }
+}
