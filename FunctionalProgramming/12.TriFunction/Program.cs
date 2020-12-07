7f10e004502c96318af78e738e2eb66ad1a51257
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-12-07 14:03:43 +0200

    Adding files

---
+++ FunctionalProgramming/12.TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Func<string, int, bool> function1 = (name, value) => 
+            name.ToCharArray().Select(ch => (int)ch).Sum() >= value;
+
+            Func<List<string>, int, Func<string, int, bool>, string> function2 =  (names, n, function1) =>
+            names.FirstOrDefault(name => function1(name, n));
+
+            Console.WriteLine(function2(names, number, function1).ToString());
+        }
+    }
+}
