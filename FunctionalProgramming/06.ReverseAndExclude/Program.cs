b0d8eca8fececfd452460e4e5c0cbe29cecce44e
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-12-07 14:01:46 +0200

    Adding files

---
+++ FunctionalProgramming/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Reverse().ToList();
+            int number = int.Parse(Console.ReadLine());
+            Func<int, bool> predicate = n => n % number != 0;
+
+            Action<List<int>> print = n => Console.WriteLine(string.Join(" ", n));
+
+            numbers = numbers.Where(predicate)
+                  .ToList();
+            print(numbers);
+        }
+    }
+}
