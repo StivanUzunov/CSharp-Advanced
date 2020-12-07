c85b2f886fa1d359b00fa81ed2cd3e8e850f5432
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-12-07 14:02:38 +0200

    Adding files

---
+++ FunctionalProgramming/09.ListOfPredicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            List<int> divisibleNumbers = new List<int>();
+
+            for (int num = 1; num <= n; num++)
+            {
+                bool isDivisible = true;
+
+                foreach (var number in numbers)
+                {
+                    Predicate<int> isNotDivisor = x => num % x != 0;
+
+                    if (isNotDivisor(number))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+                if (isDivisible)
+                {
+                    divisibleNumbers.Add(num);
+                }
+            }
+            Console.WriteLine(String.Join(" ",divisibleNumbers));
+        }
+    }
+}
