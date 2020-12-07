0550dcaf2cff1c7270e98e35f914d9f29863e381
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-12-07 13:59:36 +0200

    commit

---
+++ FunctionalProgramming/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string evenOrOdd = Console.ReadLine();
+            Predicate<int> predicate = evenOrOdd == "odd" ? 
+                new Predicate<int>((n) => n % 2 != 0) : 
+                new Predicate<int>((n) => n % 2 == 0);
+            for (int i = bounds[0]; i <= bounds[1]; i++)
+            {
+                if (predicate(i))
+                {
+                    Console.Write(i+" ");
+                }
+            }
+        }
+    }
+}
