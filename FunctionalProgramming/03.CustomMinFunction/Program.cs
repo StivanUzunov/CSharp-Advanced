ed32e572543abdadedcb0d581b997754e082918b
Stivan Uzunov <stivan.uzunov@gmail.com>
2020-12-07 13:59:13 +0200

    commit

---
+++ FunctionalProgramming/03.CustomMinFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minfunction = (arr) =>
+             {
+                 int minValue = int.MaxValue;
+                 foreach (var num in arr)
+                 {
+                     if (num<minValue)
+                     {
+                         minValue = num;
+                     }
+                 }
+                 return minValue;
+             };
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Console.WriteLine(minfunction(arr));
+        }
+    }
+}
